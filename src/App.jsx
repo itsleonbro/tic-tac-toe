296faf188b86f06f24d126d9d79f1ce4763a925b
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-03-20 17:28:48 +0200

    add route for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import styles from "./App.module.css";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import { GameProvider } from "./contexts/GameContext";
-import Header from "./components/Header/Header";
-import Home from "./components/Home/Home";
-import Game from "./components/Game/Game";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import styles from './App.module.css';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { GameProvider } from './contexts/GameContext';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Game from './components/Game/Game';
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
             <main className={styles.main}>
               <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/game" element={<Game />} />
                 <Route path="*" element={<Navigate to="/" />} />
               </Routes>
